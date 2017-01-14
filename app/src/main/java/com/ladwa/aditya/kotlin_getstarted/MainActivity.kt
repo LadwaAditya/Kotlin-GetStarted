ac0be6001ace2844710aad084b68839fd3d3f026
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-01-14 22:11:27 +0530

    Fect: Added classes and functions

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/MainActivity.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/MainActivity.kt
@@ -2,6 +2,8 @@ package com.ladwa.aditya.kotlin_getstarted
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +12,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textHello.text = "Hello Kotlin"
+
+        fun toast(string: String, length: Int = Toast.LENGTH_SHORT) {
+            Toast.makeText(this, string, length).show()
+        }
+
+        fun toast(string: String, length: Int = Toast.LENGTH_SHORT, tag: String = MainActivity::class.java.simpleName) {
+            Toast.makeText(this, "[$tag] [$string]", length).show()
+        }
+
+    }
+
+    open class Animal(name: String)
+
+    class Person(name: String, surname: String) : Animal("Dog") {
+        init {
+            Log.d("TAG", "Init")
+        }
+
+        fun add(x: Int, y: Int): Int {
+            return x + y;
+        }
+
+        fun subtract(x: Int, y: Int) = x - y
     }
 }
