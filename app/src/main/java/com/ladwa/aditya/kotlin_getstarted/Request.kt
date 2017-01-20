045c131e88a31524bedd52cd2e0b5dc59855555c
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-01-20 12:37:21 +0530

    Fect(Request): Added POJO and Gson

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/Request.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/Request.kt
@@ -1,19 +1,31 @@
 package com.ladwa.aditya.kotlin_getstarted
 
-import android.util.Log
+import com.google.gson.Gson
 import java.net.URL
-import java.util.*
 
 /**
  *
  * Created by Aditya on 20-Jan-17.
  */
-class Request(val url: String) {
+class ForecastRequest(val zipCode: String) {
 
-    fun run() {
-        val forcastJson = URL(url).readText()
-        Log.d(javaClass.simpleName, forcastJson)
+    companion object {
+        private val APP_ID = "15646a06818f61f7b8d7823ca833e1ce"
+        private val URL = "http://api.openweathermap.org/data/2.5/forecast/daily?mode=json&units=metric&cnt=7"
+        private val COMPLETE_URL = "$URL&APPID=$APP_ID&q="
     }
 
-    data class Forecast(val date: Date, val temperature: Float, val details: String)
+    fun execute(): ForecastRequest.ForecastResult {
+        val forecastJsonStr = URL(COMPLETE_URL + zipCode).readText()
+        return Gson().fromJson(forecastJsonStr, ForecastResult::class.java)
+    }
+
+
+    data class ForecastResult(val city: City, val list: List<Forecast>)
+    data class City(val id: Long, val name: String, val coord: Coordinates, val country: String, val population: Int)
+    data class Coordinates(val lon: Float, val lat: Float)
+    data class Forecast(val dt: Long, val temp: Temperature, val pressure: Float, val humidity: Int, val weather: List<Weather>, val speed: Float, val deg: Int, val clouds: Int, val rain: Float)
+    data class Temperature(val day: Float, val min: Float, val max: Float, val night: Float, val eve: Float, val morn: Float)
+    data class Weather(val id: Long, val main: String, val description: String, val icon: String)
+
 }
