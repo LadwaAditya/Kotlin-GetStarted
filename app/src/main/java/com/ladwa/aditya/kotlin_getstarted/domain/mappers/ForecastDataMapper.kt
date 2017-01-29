8011200cbbfe49ef40589c71bb708c73bb18702e
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-01-29 20:05:49 +0530

    Fect(ForecastAdapter): Changed Adapter settings

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/mappers/ForecastDataMapper.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/mappers/ForecastDataMapper.kt
@@ -2,30 +2,30 @@ package com.ladwa.aditya.kotlin_getstarted.domain.mappers
 
 import com.ladwa.aditya.kotlin_getstarted.data.Forecast
 import com.ladwa.aditya.kotlin_getstarted.data.ForecastResult
-import com.ladwa.aditya.kotlin_getstarted.domain.model.DomainClasses
+import com.ladwa.aditya.kotlin_getstarted.domain.model.ForecastList
 import java.text.DateFormat
 import java.util.*
-import com.ladwa.aditya.kotlin_getstarted.domain.model.DomainClasses.Forecast as ModelForecast
+import com.ladwa.aditya.kotlin_getstarted.domain.model.Forecast as ModelForecast
 
 /**
  *
  * Created by aditya on 29/1/17.
  */
 class ForecastDataMapper {
-    fun convertFromDataModel(forecast: ForecastResult): DomainClasses.ForecastList {
-        return DomainClasses.ForecastList(forecast.city.name, forecast.city.country,
+    fun convertFromDataModel(forecast: ForecastResult): ForecastList {
+        return ForecastList(forecast.city.name, forecast.city.country,
                 convertForecastListToDomain(forecast.list))
     }
 
     private fun convertForecastListToDomain(list: List<Forecast>):
-            List<DomainClasses.Forecast> {
+            List<ModelForecast> {
         return list.map { convertForecastItemToDomain(it) }
     }
 
-    private fun convertForecastItemToDomain(forecast: Forecast): DomainClasses.Forecast {
-        return DomainClasses.Forecast(convertDate(forecast.dt),
+    private fun convertForecastItemToDomain(forecast: Forecast): ModelForecast {
+        return ModelForecast(convertDate(forecast.dt),
                 forecast.weather[0].description, forecast.temp.max.toInt(),
-                forecast.temp.min.toInt())
+                forecast.temp.min.toInt(), generateIconUrl(forecast.weather[0].icon))
     }
 
     private fun convertDate(date: Long): String {
@@ -33,4 +33,6 @@ class ForecastDataMapper {
                 Locale.getDefault())
         return df.format(date * 1000)
     }
+
+    private fun generateIconUrl(iconCode: String): String = "http://openweathermap.org/img/w/$iconCode.png"
 }
---
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/ui/utils/ViewExtension.kt
@@ -0,0 +1,11 @@
+package com.ladwa.aditya.kotlin_getstarted.ui.utils
+
+import android.content.Context
+import android.view.View
+
+/**
+ * Created by aditya on 29/1/17.
+ */
+
+val View.ctx: Context
+    get() = context
