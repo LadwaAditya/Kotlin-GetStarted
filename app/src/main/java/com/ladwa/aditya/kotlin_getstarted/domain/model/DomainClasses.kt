101eb842f95441a14cc5c3276be6bf8a4b62be48
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-01-29 19:26:54 +0530

    Fect(ForecastAdapter): Operator Overloading

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/model/DomainClasses.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/model/DomainClasses.kt
@@ -1,12 +1,11 @@
 package com.ladwa.aditya.kotlin_getstarted.domain.model
 
-/**
- * Created by aditya on 29/1/17.
- */
-class DomainClasses {
-    data class ForecastList(val city: String, val country: String,
-                            val dailyForecast: List<Forecast>)
 
-    data class Forecast(val date: String, val description: String, val high: Int,
-                        val low: Int)
+data class ForecastList(val city: String, val country: String,
+                        val dailyForecast: List<Forecast>) {
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+    fun size(): Int = dailyForecast.size
 }
+
+data class Forecast(val date: String, val description: String, val high: Int,
+                    val low: Int)
--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/ui/adapters/ForecastListAdapter.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/ui/adapters/ForecastListAdapter.kt
@@ -3,21 +3,21 @@ package com.ladwa.aditya.kotlin_getstarted.ui.adapters
 import android.support.v7.widget.RecyclerView
 import android.view.ViewGroup
 import android.widget.TextView
-import com.ladwa.aditya.kotlin_getstarted.domain.model.DomainClasses
+import com.ladwa.aditya.kotlin_getstarted.domain.model.ForecastList
 
 /**
  *
  * Created by Aditya on 17-Jan-17.
  */
-class ForecastListAdapter(val weekForecast: DomainClasses.ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
-    override fun getItemCount(): Int = weekForecast.dailyForecast.size
+class ForecastListAdapter(val weekForecast: ForecastList) : RecyclerView.Adapter<ForecastListAdapter.ViewHolder>() {
+    override fun getItemCount(): Int = weekForecast.size()
 
     override fun onCreateViewHolder(parent: ViewGroup?, viewType: Int): ViewHolder {
         return ViewHolder(TextView(parent?.context))
     }
 
     override fun onBindViewHolder(holder: ViewHolder?, position: Int) {
-        with(weekForecast.dailyForecast[position]) {
+        with(weekForecast[position]) {
             holder!!.textView.text = "$date - $description - $high/$low"
         }
     }
