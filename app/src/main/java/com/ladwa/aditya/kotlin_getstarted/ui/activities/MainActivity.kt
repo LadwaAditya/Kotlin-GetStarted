93e5825787bd8ff68bf13b6ac5e178052aaa50c8
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-01 17:31:06 +0530

    Refactor(Main): synthetic import for main activity

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/ui/activities/MainActivity.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/ui/activities/MainActivity.kt
@@ -4,28 +4,29 @@ import android.content.Context
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
 import android.util.Log
 import android.widget.Toast
 import com.ladwa.aditya.kotlin_getstarted.R
 import com.ladwa.aditya.kotlin_getstarted.domain.commands.RequestForecastCommand
 import com.ladwa.aditya.kotlin_getstarted.ui.adapters.ForecastListAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.toast
 import org.jetbrains.anko.uiThread
+
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val foreCastList = findViewById(R.id.list) as RecyclerView
-        foreCastList.layoutManager = LinearLayoutManager(this)
-        foreCastList.setHasFixedSize(true)
+
+        forecastList.layoutManager = LinearLayoutManager(this)
+        forecastList.setHasFixedSize(true)
         doAsync {
             val result = RequestForecastCommand("94043").execute()
             uiThread {
-                foreCastList.adapter = ForecastListAdapter(result) { toast(it.date) }
+                forecastList.adapter = ForecastListAdapter(result) { toast(it.date) }
                 Log.d("Tag", "Received data")
             }
         }
