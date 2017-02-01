19e95f9147e44d255e078f8589a0fb001d7db9d6
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-02-01 17:14:28 +0530

    Refactor(Main): Modifiers and Visibility properties

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/commands/RequestForecastCommand.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/commands/RequestForecastCommand.kt
@@ -5,9 +5,10 @@ import com.ladwa.aditya.kotlin_getstarted.domain.mappers.ForecastDataMapper
 import com.ladwa.aditya.kotlin_getstarted.domain.model.ForecastList
 
 /**
+ *
  * Created by aditya on 29/1/17.
  */
-class RequestForecastCommand(val zipCode: String) : Command<ForecastList> {
+class RequestForecastCommand(private val zipCode: String) : Command<ForecastList> {
     override fun execute(): ForecastList {
         val forecastRequest = ForecastRequest(zipCode)
         return ForecastDataMapper().convertFromDataModel(forecastRequest.execute())
