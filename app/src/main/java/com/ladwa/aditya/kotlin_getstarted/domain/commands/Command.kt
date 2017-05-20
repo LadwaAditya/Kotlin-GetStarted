7a2de01e50335370286d20f23d699d6a7031406d
Aditya Ladwa <ladwa.aditya@gmail.com>
2017-05-20 23:15:00 +0530

    Version bump

--- app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/commands/Command.kt
+++ app/src/main/java/com/ladwa/aditya/kotlin_getstarted/domain/commands/Command.kt
@@ -3,6 +3,6 @@ package com.ladwa.aditya.kotlin_getstarted.domain.commands
 /**
  * Created by Aditya on 20-Jan-17.
  */
-public interface Command<T> {
+interface Command<T> {
     fun execute(): T
 }
